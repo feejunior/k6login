dd166cc4d6d6e7d5719e0378d87430b897ae93e4
Felipe Junior <132328323+feejunior@users.noreply.github.com>
2024-07-30 12:07:29 -0300

    Update k6login.js

--- k6login.js
+++ k6login.js
@@ -2,7 +2,7 @@ import http from 'k6/http'
 import { sleep, check } from 'k6'
 
 export default function () {
-    const url = 'your_url'
+    const url = 'sua.url'
     
     // Vai o token que pegamos na url 
     const headersApp = { 
@@ -12,7 +12,7 @@ export default function () {
         }
     }
     // Requisição pra gerar o token de aplicação 
-    const resApp = http.get(url + "caminho/para/gerar/api", headersApp)
+    const resApp = http.get(url + 'caminho/para/gerar/api', headersApp)
     // Atribuída a resposta da requisição pra essa constante
     const bodyApp = resApp.body
 
@@ -30,7 +30,7 @@ export default function () {
     }
     
     // Estrutura de requisição que vai fazer a autenticação do usuário
-    const resLogin = http.post(url + "caminho/para/autentica/usuário", payloadLogin, headersLogin)
+    const resLogin = http.post(url + 'caminho/para/autentica/usuário', payloadLogin, headersLogin)
     
     // Imprime o corpo da requisição, se tudo ocorrer bem, retornará o que foi setado
     console.log(resApp.body)
